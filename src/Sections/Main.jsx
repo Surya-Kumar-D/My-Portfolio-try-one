3aba5e7a53bb8ee51d8c655661393cdc384c805a
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-08-28 17:44:28 +0530

    tried to add responsive design

--- src/Sections/Main.jsx
+++ src/Sections/Main.jsx
@@ -3,19 +3,19 @@ import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
 
 function Main() {
   return (
-    <div className="grid h-dvh grid-cols-[60rem_auto] bg-[#61dafb1f]">
-      <div className="flex items-center justify-center">
+    <div className="grid h-svh w-full bg-[#61dafb1f] md:grid-cols-[60rem_auto]">
+      <div className="order-2 flex items-center justify-center md:order-1">
         <img
-          className="mix-blend-multiply"
+          className="block mix-blend-multiply"
           src="./developer.jpg"
           alt="Developer-image"
         />
       </div>
-      <div className="flex items-center justify-center space-x-3 text-[5rem] text-[#61DAFB]">
-        <DiReact size={300} />
+      <div className="order-1 flex items-center justify-center space-x-3 text-[#61DAFB] md:order-2 md:text-[5rem]">
+        <DiReact size={200} className="text-[100] md:text-[300]" />
         <div>
           <FaQuoteLeft size={20} />
-          <span>I am a React Developer</span>
+          <span className="text-2xl">I am Surya Kumar</span>
           <FaQuoteRight size={20} />
         </div>
       </div>
